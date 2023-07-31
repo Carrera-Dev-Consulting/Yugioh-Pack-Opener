7acbe2eb0127f5aa0471e937d97e83c03e34c9e3
ANDRES H CARRERA <andreshcar@live.com>
2023-07-31 16:00:21 -0600

    Refactoring backend (#22)
    Refactored backend to be in seperate files instead of a single one.

---
+++ backend/app/entrypoint/fastapi_app/__init__.py
@@ -0,0 +1,15 @@
+from logging import getLogger
+from fastapi import FastAPI
+import uvicorn
+
+from app.services.logging import configure_logging
+from .endpoints import api_router
+
+app = FastAPI()
+app.include_router(api_router)
+
+def main():
+    configure_logging()
+    logger = getLogger(__name__)
+    logger.info('Starting up server')
+    uvicorn.run(app, host='0.0.0.0', port=8080)
