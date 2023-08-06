cc60256a9760d01fb11531970bd8ad73ec8c64dd
Alexander Quintanilla <140995210+vCertxfiedGoat@users.noreply.github.com>
2023-08-06 10:26:51 -0600

    made visible non final since it represents whether you can see the card or not.

--- frontend/mobile/packopener/lib/widgets/base/playing_card.dart
+++ frontend/mobile/packopener/lib/widgets/base/playing_card.dart
@@ -2,8 +2,9 @@ import 'package:flutter/widgets.dart';
 
 abstract class PlayingCard extends StatelessWidget {
   final double size;
-  final bool visible;
-  const PlayingCard({super.key, this.size = 1, this.visible = false});
+  bool visible;
+
+  PlayingCard({super.key, this.size = 1, this.visible = false});
 
   @override
   Widget build(BuildContext context) {
