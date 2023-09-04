30284e030d189faa3fbae7e2a9573952ba0e4b57
Andres Hermilo Carrera <andreshcar@live.com>
2023-09-04 11:37:01 -0600

    Adding doc strings to the gacha stuff because it would be a good idea

--- backend/app/models/gacha_configuration.py
+++ backend/app/models/gacha_configuration.py
@@ -1,40 +1,94 @@
+from dataclasses import dataclass, field
 import random
+from typing import Protocol
 
 from .base import Base
 from .yugioh_set import YugiohCardInSet
 from .gacha_pull import GachaPull
 
 
-class Pool(Base):
+class SingleRarityPool(Base):
+    """Defines a pool of a single rarity that will be used for situtations like a set amount of common and rare cards
+
+    Args:
+        quantity (int): Amount of cards that will be pulled from the collection
+        cards (list[YugiohCardInSet]): Cards in pool that can be selected.
+    """
+
     quantity: int
     cards: list[YugiohCardInSet]
 
-    def _pick_card(self):
-        index = random.randrange(0, len(self.cards))
+    def _pick_card(self, random_range=random.randrange):
+        index = random_range(0, len(self.cards))
         pick = self.cards[index]
         del self.cards[index]
         return pick
 
-    def pull(self) -> list[YugiohCardInSet]:
-        return [self._pick_card() for _ in range(self.quantity)]
+    def pull(self, random_range=random.randrange) -> list[YugiohCardInSet]:
+        """Generates a pull from the collection of cards that are here for the amount defined in quantity.
+
+        Args:
+            random_range (random.randrange, optional): Random function used to select value in collection. Defaults to random.randrange.
+
+        Returns:
+            list[YugiohCardInSet]: List of cards that have been removed from the pool
+        """
+        return [
+            self._pick_card(random_range=random_range) for _ in range(self.quantity)
+        ]
 
 
-class Weight(Base):
+class WeightedCollection(Base):
+    """Weighted collection of cards used by the WeightedRarityPool that exposes a way to select a single
+    card from the selection
+
+    Args:
+        cards (list[YugiohCardInSet]): cards in the collection
+        weight (float): the value representing how frequently this item should appear
+    """
+
     cards: list[YugiohCardInSet]
     weight: float
 
-    def pick(self) -> YugiohCardInSet:
-        index = random.randrange(0, len(self.cards))
+    def pick(self, random_range=random.randrange) -> YugiohCardInSet:
+        """Selects and removes a YugiohCardInSet object from the backing collection.
+        Args:
+            random_range (random.randrange, optional): Random function used to select the specific card from the collection. Defaults to random.randrange.
+
+        Returns:
+            YugiohCardInSet: card that was selected randomly
+        """
+        index = random_range(0, len(self.cards))
         pick = self.cards[index]
         del self.cards[index]
         return pick
 
 
-class WeightedPool(Base):
-    quantity: int
-    weights: list[Weight]
+class WeightedRarityPool(Base):
+    """Pool that uses a collection of weights that allows cards to be picked from their subset of cards.
+    This is used when we have selection of rarities for a specific slot in a pack for the higher rarity cards
+    that could also be commons.
 
-    def pick_weight(self, random_float=random.uniform) -> Weight:
+    Args:
+        quantity (int): amount of cards that will be generated from the pull method
+        weights (list[Weight]): list of different weights that have been defined in pool
+    """
+
+    quantity: int
+    weights: list[WeightedCollection]
+
+    def pick_weight(self, random_float=random.uniform) -> WeightedCollection:
+        """Selects the specific weight that will be used in the pull for a specific pick
+
+        Args:
+            random_float (random.uniform, optional): random_function that is used to determine. Defaults to random.uniform.
+
+        Raises:
+            ValueError: _description_
+
+        Returns:
+            WeightedCollection: _description_
+        """
         total_weight = sum([weight.weight for weight in self.weights])
         if total_weight == 0:
             raise ValueError("No items to weigh")
@@ -46,13 +100,35 @@ class WeightedPool(Base):
             if current_weight > selected_value:
                 return weight
 
-    def pull(self) -> list[YugiohCardInSet]:
-        return [self._pick_weight().pick() for _ in range(self.quantity)]
+    def pull(
+        self, random_float=random.uniform, random_range=random.randrange
+    ) -> list[YugiohCardInSet]:
+        """Generate a pull for the gacha system from the weighted collections using the weights to determine how rarities are selected.
+
+        Args:
+            random_float (random.uniform, optional): function to generate random float which is used to select the specific weights. Defaults to random.uniform.
+            random_range (random.randrange, optional): function to generate random int from range which is used to select card from weighted pool. Defaults to random.randrange.
+
+        Returns:
+            list[YugiohCardInSet]: Selected cards for the most recent pull from the pool
+        """
+        return [
+            self.pick_weight(random_float=random_float).pick(random_range=random_range)
+            for _ in range(self.quantity)
+        ]
 
 
-class GachaConfiguration(Base):
-    card_pools: list[Pool | WeightedPool]
+class Pool(Protocol):
+    """Definition of Card Pool which allows people to pull cards based on pool configuration"""
+
+    def pull() -> list[YugiohCardInSet]:
+        pass
+
+
+@dataclass
+class GachaConfiguration:
+    pools: list[Pool] = field()
 
     def pull(self) -> GachaPull:
-        cards = [card.card_id for pool in self.card_pools for card in pool.pull()]
+        cards = [card.card_id for pool in self.pools for card in pool.pull()]
         return GachaPull(results=cards)
--- backend/tests/test_app/test_models/test_gacha_configuration/test_weighted_pool.py
+++ backend/tests/test_app/test_models/test_gacha_configuration/test_weighted_pool.py
@@ -1,10 +1,10 @@
 from unittest.mock import Mock
 import pytest
-from app.models.gacha_configuration import Weight, WeightedPool
+from app.models.gacha_configuration import WeightedCollection, WeightedRarityPool
 
 
 def test_when_weighted_pool_total_does_not_work__raise_value_error():
-    pool = WeightedPool(quantity=1, weights=[])
+    pool = WeightedRarityPool(quantity=1, weights=[])
     with pytest.raises(ValueError):
         pool.pick_weight()
 
@@ -15,9 +15,9 @@ def random():
 
 
 def test_when_random_selects_less_then_light_weight__picks_light_weight_item(random):
-    low_weight = Weight(cards=[], weight=0.1)
-    high_weight = Weight(cards=[], weight=0.9)
-    sut = WeightedPool(quantity=1, weights=[low_weight, high_weight])
+    low_weight = WeightedCollection(cards=[], weight=0.1)
+    high_weight = WeightedCollection(cards=[], weight=0.9)
+    sut = WeightedRarityPool(quantity=1, weights=[low_weight, high_weight])
     random.return_value = 0.01
     picked = sut.pick_weight(random_float=random)
 
@@ -27,9 +27,9 @@ def test_when_random_selects_less_then_light_weight__picks_light_weight_item(ran
 def test_when_random_selects_exactly_same_as_low_weight_weight__picks_high_weight_item(
     random,
 ):
-    low_weight = Weight(cards=[], weight=0.1)
-    high_weight = Weight(cards=[], weight=0.9)
-    sut = WeightedPool(quantity=1, weights=[low_weight, high_weight])
+    low_weight = WeightedCollection(cards=[], weight=0.1)
+    high_weight = WeightedCollection(cards=[], weight=0.9)
+    sut = WeightedRarityPool(quantity=1, weights=[low_weight, high_weight])
     random.return_value = 0.1
     picked = sut.pick_weight(random_float=random)
 
@@ -39,9 +39,9 @@ def test_when_random_selects_exactly_same_as_low_weight_weight__picks_high_weigh
 def test_when_random_selects_less_then_light_weight_by_a_single_percentage__picks_light_weight_item(
     random,
 ):
-    low_weight = Weight(cards=[], weight=0.1)
-    high_weight = Weight(cards=[], weight=0.9)
-    sut = WeightedPool(quantity=1, weights=[low_weight, high_weight])
+    low_weight = WeightedCollection(cards=[], weight=0.1)
+    high_weight = WeightedCollection(cards=[], weight=0.9)
+    sut = WeightedRarityPool(quantity=1, weights=[low_weight, high_weight])
     random.return_value = 0.09
     picked = sut.pick_weight(random_float=random)
 
@@ -51,9 +51,9 @@ def test_when_random_selects_less_then_light_weight_by_a_single_percentage__pick
 def test_when_random_selects_less_then_light_weight_by_two_nines__picks_light_weight_item(
     random,
 ):
-    low_weight = Weight(cards=[], weight=0.1)
-    high_weight = Weight(cards=[], weight=0.9)
-    sut = WeightedPool(quantity=1, weights=[low_weight, high_weight])
+    low_weight = WeightedCollection(cards=[], weight=0.1)
+    high_weight = WeightedCollection(cards=[], weight=0.9)
+    sut = WeightedRarityPool(quantity=1, weights=[low_weight, high_weight])
     random.return_value = 0.01
     picked = sut.pick_weight(random_float=random)
 
@@ -61,9 +61,9 @@ def test_when_random_selects_less_then_light_weight_by_two_nines__picks_light_we
 
 
 def test_when_random_selects_less_then_light_weight__picks_light_weight_item(random):
-    low_weight = Weight(cards=[], weight=0.1)
-    high_weight = Weight(cards=[], weight=0.9)
-    sut = WeightedPool(quantity=1, weights=[low_weight, high_weight])
+    low_weight = WeightedCollection(cards=[], weight=0.1)
+    high_weight = WeightedCollection(cards=[], weight=0.9)
+    sut = WeightedRarityPool(quantity=1, weights=[low_weight, high_weight])
     random.return_value = 0.01
     picked = sut.pick_weight(random_float=random)
 
@@ -73,10 +73,12 @@ def test_when_random_selects_less_then_light_weight__picks_light_weight_item(ran
 def test_when_random_selects_gets_one_greater_then_first_percentage_but_less_than_second__returns_second_weight(
     random,
 ):
-    low_weight = Weight(cards=[], weight=0.1)
-    other_low_weight = Weight(cards=[], weight=0.2)
-    high_weight = Weight(cards=[], weight=0.7)
-    sut = WeightedPool(quantity=1, weights=[low_weight, high_weight, other_low_weight])
+    low_weight = WeightedCollection(cards=[], weight=0.1)
+    other_low_weight = WeightedCollection(cards=[], weight=0.2)
+    high_weight = WeightedCollection(cards=[], weight=0.7)
+    sut = WeightedRarityPool(
+        quantity=1, weights=[low_weight, high_weight, other_low_weight]
+    )
     random.return_value = 0.1
     picked = sut.pick_weight(random_float=random)
 
@@ -86,10 +88,12 @@ def test_when_random_selects_gets_one_greater_then_first_percentage_but_less_tha
 def test_when_random_selects_gets_one_greater_then_first_percentage_but_less_than_second__returns_second_weight(
     random,
 ):
-    low_weight = Weight(cards=[], weight=0.1)
-    other_low_weight = Weight(cards=[], weight=0.2)
-    high_weight = Weight(cards=[], weight=0.7)
-    sut = WeightedPool(quantity=1, weights=[low_weight, high_weight, other_low_weight])
+    low_weight = WeightedCollection(cards=[], weight=0.1)
+    other_low_weight = WeightedCollection(cards=[], weight=0.2)
+    high_weight = WeightedCollection(cards=[], weight=0.7)
+    sut = WeightedRarityPool(
+        quantity=1, weights=[low_weight, high_weight, other_low_weight]
+    )
     random.return_value = 0.1
     picked = sut.pick_weight(random_float=random)
 
@@ -99,10 +103,12 @@ def test_when_random_selects_gets_one_greater_then_first_percentage_but_less_tha
 def test_when_random_selects_equal_to_second_position__returns_third_weight(
     random,
 ):
-    low_weight = Weight(cards=[], weight=0.1)
-    other_low_weight = Weight(cards=[], weight=0.2)
-    high_weight = Weight(cards=[], weight=0.7)
-    sut = WeightedPool(quantity=1, weights=[low_weight, high_weight, other_low_weight])
+    low_weight = WeightedCollection(cards=[], weight=0.1)
+    other_low_weight = WeightedCollection(cards=[], weight=0.2)
+    high_weight = WeightedCollection(cards=[], weight=0.7)
+    sut = WeightedRarityPool(
+        quantity=1, weights=[low_weight, high_weight, other_low_weight]
+    )
     random.return_value = 0.3
     picked = sut.pick_weight(random_float=random)
 
@@ -110,14 +116,14 @@ def test_when_random_selects_equal_to_second_position__returns_third_weight(
 
 
 def test_when_weights_are_greater_than_one__still_selects_weights_properly(random):
-    low_weight = Weight(cards=[], weight=0.16)
-    other_low_weight = Weight(cards=[], weight=0.14285)
-    other_lowish_weight = Weight(cards=[], weight=0.2)
-    high_weight = Weight(cards=[], weight=1)
+    low_weight = WeightedCollection(cards=[], weight=0.16)
+    other_low_weight = WeightedCollection(cards=[], weight=0.14285)
+    other_lowish_weight = WeightedCollection(cards=[], weight=0.2)
+    high_weight = WeightedCollection(cards=[], weight=1)
 
     random.return_value = 0.5
 
-    sut = WeightedPool(
+    sut = WeightedRarityPool(
         quantity=1,
         weights=[low_weight, other_low_weight, other_lowish_weight, high_weight],
     )
