1d600df0a68e84a3b98fa15b937d6f04433e7a44
Alexander Quintanilla <140995210+vCertxfiedGoat@users.noreply.github.com>
2023-09-05 13:04:47 -0600

    Making the application a full screen app

--- frontend/mobile/packopener/lib/main.dart
+++ frontend/mobile/packopener/lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
-import 'widgets/cards/yugioh_card.dart';
+import "package:flutter/services.dart";
+import 'pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack,
+      overlays: [SystemUiOverlay.bottom]);
+  SystemChrome.setSystemUIChangeCallback((systemOverlaysAreVisible) {
+    return SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack,
+        overlays: [SystemUiOverlay.bottom]);
+  });
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
+      .then((value) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -14,84 +24,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Pack Opener',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+        colorScheme: const ColorScheme(
+          brightness: Brightness.dark,
+          background: Color.fromARGB(255, 41, 47, 54),
+          primary: Color.fromARGB(255, 78, 205, 196),
+          secondary: Color.fromARGB(255, 247, 255, 247),
+          tertiary: Color.fromARGB(255, 255, 107, 107),
+          onBackground: Color.fromARGB(255, 247, 255, 247),
+          error: Color.fromARGB(255, 41, 47, 54),
+          onError: Color.fromARGB(255, 255, 107, 107),
+          onPrimary: Color.fromARGB(255, 247, 255, 247),
+          onSecondary: Color.fromARGB(255, 41, 47, 54),
+          onSurface: Color.fromARGB(255, 41, 47, 54),
+          surface: Color.fromARGB(255, 78, 205, 196),
+        ),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Pack Opener'),
+      home: const MyHomePage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  bool cardIsVisible = false;
-  _clickCard() {
-    setState(() {
-      cardIsVisible = cardIsVisible ? false : true;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    YugiohCard? card = YugiohCard(
-      click: _clickCard,
-      visible: cardIsVisible,
-    );
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-        appBar: AppBar(
-          // TRY THIS: Try changing the color here to a specific color (to
-          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-          // change color while the other colors stay the same.
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
-        body: Center(child: card
-            // Center is a layout widget. It takes a single child and positions it
-            // in the middle of the parent.
-            // floatingActionButton: FloatingActionButton(
-            //   onPressed: _pullCard,
-            //   tooltip: 'Increment',
-            //   child: const Icon(Icons.add),
-            )); // This trailing comma makes auto-formatting nicer for build methods.
   }
 }
--- frontend/mobile/packopener/lib/models/packs/pack_model.dart
+++ frontend/mobile/packopener/lib/models/packs/pack_model.dart
@@ -1,28 +1,9 @@
 // import 'package:packopener/models/cards/card_model.dart';
-// import 'package:packopener/models/enums/game.dart';
 
 // class PackModel {
 //   final List<CardModel> cards;
 //   final int price;
 //   final String images;
-
-//   factory PackModel.fromJson(Map<String, dynamic> json) {
-//     dynamic cardblobs = json["cards"];
-//     for (int i = 0; i < cardblobs.length; i++) {}
-//     List<String> images = List.empty(growable: true);
-//     for (var i = 0; i < cardblobs["card_images"].length; i++) {
-//       images.add(cardblobs["card_images"][i]["image_url"].toString());
-//       images.add(cardblobs["card_images"][i]["image_url_small"].toString());
-//     }
-
-//     return YugiohCardModel(
-//         level: int.parse(cardblobs["level"].toString()),
-//         attribute: cardblobs["attribute"].toString(),
-//         attack: int.parse(cardblobs["attack"].toString()),
-//         defense: int.parse(cardblobs["defense"].toString()),
-//         name: cardblobs["name"].toString(),
-//         description: cardblobs["description"],
-//         type: cardblobs["type"].toString(),
-//         images: images);
-//   }
+//   const PackModel({required this.cards, required this.images, required this.price})
+  
 // }
