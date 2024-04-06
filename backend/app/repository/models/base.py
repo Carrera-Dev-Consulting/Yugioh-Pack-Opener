22d6fa2ff197825b658c6e0d3407dafe1821f347
Milo <andreshcar@live.com>
2024-04-06 12:42:41 -0600

    Adding stuf

--- backend/app/repository/models/base.py
+++ backend/app/repository/models/base.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import declarative_base
 BaseSQLModel = declarative_base()
 
 
-def base_model_str(self):
+def base_model_str(self) -> str:
     set_values = ",".join(f"{key}={getattr(self, key)}" for key in self.__columns__)
     return f"{type(self)}({set_values})"
 
